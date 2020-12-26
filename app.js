8494951ad3c738465d5512e410fc294ba29657c1
Jeet Viramgama <64732692+jv18creator@users.noreply.github.com>
2020-12-26 17:42:26 +0530

    Update 1.3

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 let hamburger = document.getElementById('hamburger');
 let closeOp = document.getElementById('close-op');
-let navbarLinks = document.querySelectorAll('.link')
-console.log(navbarLinks)
+let navbarLinks = document.querySelectorAll('.link');
+let navContainer = document.querySelector('.container-links');
+console.log(navContainer)
 
 closeOp.style.display = 'none';
-
 
 hamburger.addEventListener('click', ()=>{
     hamburger.style.display = 'none';
     for (let i = 0; i < navbarLinks.length; i++) {
         navbarLinks[i].style.display = 'block';
-        navbarLinks[i].style.backgroundColor = "#BADA55";
+       // navContainer.style.backgroundColor = "#BADA55";
+        //navContainer.style.transform = `translateY(200px)`
+       // navbarLinks[i].style.backgroundColor = "#BADA55";
     }
     closeOp.style.display = 'block';
 });
